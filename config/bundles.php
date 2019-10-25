2209c5ba6d986b27f9da3f4aae2b661136fabd1d
Raphaël Gertz <git@rapsys.eu>
2019-10-25 18:20:54 +0200

    Import bundle file

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Rapsys\PackBundle\RapsysPackBundle::class => ['all' => true],
+    Rapsys\UserBundle\RapsysUserBundle::class => ['all' => true],
+    Rapsys\AirBundle\RapsysAirBundle::class => ['all' => true],
+];
