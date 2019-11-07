c4dee631491e8e9ec4bcb65191f22cd72d3b83ab
Raphaël Gertz <git@rapsys.eu>
2019-11-07 04:15:29 +0100

    Remove config/bundles.php from repository

--- config/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Rapsys\PackBundle\RapsysPackBundle::class => ['all' => true],
-    Rapsys\UserBundle\RapsysUserBundle::class => ['all' => true],
-    Rapsys\AirBundle\RapsysAirBundle::class => ['all' => true],
-];
